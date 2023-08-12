90687670c78b31d16059dcc04839876baa1df152
Tabrez Iqbal <tabrez@mailbox.org>
2023-08-12 17:28:25 +0530

    add full code for the file 08_Collaborative_Filtering.py

--- 08_Collaborative_Filtering.py
+++ 08_Collaborative_Filtering.py
@@ -38,7 +38,7 @@ dls = CollabDataLoaders.from_df(ratings, item_name='title', bs=64)
 dls.show_batch()
 
 #%% inspect classes in dls
-print(type(dls.classes), len(dls.classes['user']), len(dls.classes['title']))
+print(type(dls.classes), len(dls.classes['user']), dls.classes['user'][:5], len(dls.classes['title']), dls.classes['title'][:5])
 dls.classes['user'][:5], dls.classes['title'][:5]
 
 #%% representation for movie and user latent factors
@@ -47,4 +47,98 @@ n_movies = len(dls.classes['title'])
 n_factors = 5
 user_factors = torch.randn(n_users, n_factors)
 movie_factors = torch.randn(n_movies, n_factors)
+
+print(f'user_factors[:5]: {user_factors[:5]}')
+print(f'movie_factors[:5]: {movie_factors[:5]}')
+
+#%% indexing with vectors
+help(one_hot)
+one_hot_3 = one_hot(3, n_users).float()
+# print(f'one_hot_3: {one_hot_3}')
+user_factor_3 = user_factors.T @ one_hot_3
+print(f'user_factor_3: {user_factor_3}, user_factors[3]: {user_factors[3]}')
+
+#%% some indexing tricks with pd.DataFrame
+df1 = pd.DataFrame(data=[1,2,3,4,5,6,7,7,7])
+df1.iloc[[6,6,6,6,6,1,6]]
+df1[0], df1[[0]]
+batch = tensor([[0,7],
+         [0,6],
+         [1,7],
+         [3,1]])
+batch[:,0], batch[:,1]
+df1.iloc[batch[:,0]], df1.iloc[[0,0,1,3]]
+
+x,y = dls.one_batch()
+print(f'x.shape: {x.shape}')
+
+print(f'product', batch[:,0]*batch[:,1])
+
+
+#%% create our custom model
+class DotProduct1(Module):
+  def __init__(self, n_users, n_movies, n_factors, y_range=(0, 5.5)):
+    self.user_factors = Embedding(n_users, n_factors)
+    self.movie_factors = Embedding(n_movies, n_factors)
+    self.y_range = y_range
+
+  def forward(self, batch):
+    users = self.user_factors(batch[:,0])
+    movies = self.movie_factors(batch[:,1])
+    dotp = (users * movies).sum(dim=1)
+    return sigmoid_range(dotp, *self.y_range)
+
+class DotProduct2(Module):
+  def __init__(self, n_users, n_movies, n_factors, y_range=(0, 5.5)):
+    self.user_factors = Embedding(n_users, n_factors)
+    self.movie_factors = Embedding(n_movies, n_factors)
+    self.y_range = y_range
+    self.user_bias = Embedding(n_users, 1)
+    self.movie_bias = Embedding(n_movies, 1)
+
+  def forward(self, batch):
+    users = self.user_factors(batch[:,0])
+    movies = self.movie_factors(batch[:,1])
+    user_bias = self.user_bias(batch[:,0])
+    movie_bias = self.movie_bias(batch[:,1])
+    # print('======================================')
+    # print(f'users: {users}, \nmovies: {movies},\n users*movies: {users*movies}')
+    # print(f'sum: {(users*movies).sum(dim=1, keepdim=True)}, \nuser_bias: {user_bias}, \nmovie_bias: {movie_bias}')
+    dotp = (users * movies).sum(dim=1, keepdim=True) + user_bias + movie_bias
+    # print(f'dotp: { dotp}')
+    return sigmoid_range(dotp.sum(dim=1), *self.y_range)
+
+#%% Use pytorch tensors instead of Embedding type
+def create_params(size):
+  return nn.Parameter(torch.randn(*size).normal_(0, 0.01))
+
+class DotProduct(Module):
+  def __init__(self, n_users, n_movies, n_factors, y_range=(0, 5.5)):
+    self.user_factors = create_params((n_users, n_factors))
+    self.movie_factors = create_params((n_movies, n_factors))
+    self.y_range = y_range
+    self.user_bias = create_params((n_users, 1)# wrong:)
+    self.movie_bias = create_params((n_movies, 1))
+
+  def forward(self, batch):
+    users = self.user_factors[batch[:,0]]
+    movies = self.movie_factors[batch[:,1]]
+    user_bias = self.user_bias[batch[:,0]]
+    movie_bias = self.movie_bias[batch[:,1]]
+    dotp = (users * movies).sum(dim=1, keepdim=True) + user_bias + movie_bias
+    return sigmoid_range(dotp.sum(dim=1), *self.y_range)
+
+model = DotProduct(n_users, n_movies, 50)
+learn = Learner(dls, model, loss_func=MSELossFlat(), wd=0.2)
+learn.fit_one_cycle(5, 5e-3)
+
+
+#%% check model
+len(learn.model.movie_bias), len(learn.model.movie_bias.squeeze())
+learn.model.movie_bias, learn.model.movie_bias.squeeze()
+idxs1 = learn.model.movie_bias.squeeze().argsort()[:5]
+# wrong:
+# idxs1.tolist(), movies[movies['movie'].isin(idxs1.tolist())]
+idxs2 = learn.model.movie_bias.squeeze().argsort()[-5:]
+dls.classes['title'][idxs1], dls.classes['title'][idxs2],
 
