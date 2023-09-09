2c460500783083b17bc2ab583c023c7851429d79
Tabrez Iqbal <tabrez@mailbox.org>
2023-09-09 15:26:09 +0530

    add a slightly more complext example of a non-linear function

--- smol/02_parameter_learning_pytorch.py
+++ smol/02_parameter_learning_pytorch.py
@@ -7,9 +7,9 @@ class LinearRegression(t.nn.Module):
           # try different number of activations below: 1, 2, 3, 10
           # loss is good on some runs but not the others with lower numbers like 1, 2 - why?
           # Try with fixed seed for parameters created by nn.LinearRegression
-          t.nn.Linear(n_features, 3),
+          t.nn.Linear(n_features, 1),
           t.nn.ReLU(),
-          t.nn.Linear(3, n_outputs))
+          t.nn.Linear(1, n_outputs))
 
     def forward(self, x):
         return self.linear(x)
@@ -20,16 +20,18 @@ def driver(n_epochs=10, lr=0.1):
   optimizer = t.optim.SGD(model.parameters(), lr)
 
   # x1 = t.linspace(start=-1, end=1, steps=90).view(-1, 1).float()
-  x1 = t.randn(90).view(-1, 1)
-  x2 = x1*x1
-  x3 = x1*x1*x1
-  y = (122 * x1) + 73 * x2 + 12 * x3 + 12
-  # print(f'y: {y}')
-  # y_mean = y.mean()
-  # y_std = y.std()
-  # y = (y - y_mean) / y_std
-  print(f'y: {y}')
+  # x1 = t.randn(90).view(-1, 1)
+  # x2 = x1*x1
+  # x3 = x1*x1*x1
+  # y = (122 * x1) + 73 * x2 + 12 * x3 + 12
 
+  x1 = t.randn(9).view(-1, 1)
+  x2 = t.randn(9).view(-1, 1)
+  x3 = t.randn(9).view(-1, 1)
+  y = t.sigmoid(x1) * t.sin(2 * t.math.pi * x2) + t.log(t.clamp(x3, min=1e-7) + 1)
+
+
+  print(f'y: {y}')
   for epoch in range(n_epochs):
       y_pred = model(t.cat((x1, x2, x3), dim=1))
       loss = mse_loss(y_pred, y)
@@ -45,7 +47,7 @@ def driver(n_epochs=10, lr=0.1):
 
   return loss, model
 
-lossf, model = driver(950, 0.0001)
+lossf, model = driver(30, 0.1)
 print('++++++++++++++++++++++++++++++++++++++++++++++')
 print(f'final loss: {lossf}')
 print(f'params: {list(model.parameters())}')
@@ -57,6 +59,16 @@ x2 = x1*x1
 x3 = x1*x1*x1
 new_y = (122 * x1) + 73 * x2 + 12 * x3 + 12
 # new_y = 122 * 0.3 + 73 * 0.09 + 12 * 0.027 + 12
+print(f'new_y: {new_y}')
+pred_y = model(t.tensor([x1, x2, x3]))
+print(f'pred_y: {pred_y}')
+
+#%%
+x1 = t.randn(1).view(-1, 1)
+x2 = t.randn(1).view(-1, 1)
+x3 = t.randn(1).view(-1, 1)
+new_y = t.sigmoid(x1) * t.sin(2 * t.math.pi * x2) + t.log(t.clamp(x3, min=1e-7) + 1)
+
 print(f'new_y: {new_y}')
 pred_y = model(t.tensor([x1, x2, x3]))
 print(f'pred_y: {pred_y}')
