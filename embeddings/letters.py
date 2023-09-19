b2e2fdf089c30480bdcec151d49f38f2702f363f
Tabrez Iqbal <tabrez@mailbox.org>
2023-09-19 15:32:32 +0530

    apply some refactoring, move extra code to botton of the file

--- embeddings/letters.py
+++ embeddings/letters.py
@@ -1,67 +1,53 @@
 #%% import packages
-# import spacy
 import string
 from collections import Counter
-
-#%% prepare some raw text with only lower case letters, digits and underscore
-with open('rod_steiger.txt', 'r') as file:
-  text = file.read()
-
-text = text.lower()
-# print(text)
-table = str.maketrans('\n\t -ˈ—éəɪɡ–', '___________', string.punctuation)
-text = text.translate(table)
-# print(text)
-
-print(f'len of text: {len(text)}, text[:20]: {text[:20]}')
-
-#%% create the dataset - version 1
-vocab = sorted(list(set(text)))
-# vocab = sorted(set(text))
-print(f'len of vocab: {len(vocab)}\n\nvocab: {vocab}\n\ncount: {Counter(text)}')
-
-ds1 = []
-# 0th element is not present in the input and last element is not present in the output
-# alternatively add a dummy element at the start and the end of the text
-for i in range(len(text)-1):
-  pair = (text[i], text[i+1])
-  ds1.append(pair)
-
-print(f'len of ds1: {len(ds1)}, firt few: {ds1[:5]}')
-#%% create the dataset - version 2
-ds2 = { 'input': [], 'output': []}
-for i in range(len(text)-1):
-  ds2['input'].append(text[i])
-  ds2['output'].append(text[i+1])
-
-print(f'len of input: {len(ds2["input"])}, len of output: {len(ds2["output"])}')
-
-#%% create the dataset with numericalisation - version 3
-vocab = sorted(list(set(text)))
-print(f'len of vocab: {len(vocab)}\n\nvocab: {vocab}\n\ncount: {Counter(text)}')
-
-nums = []
-for i in range(len(text)):
-  index = vocab.index(text[i])
-  nums.append(index)
-
-ds = { 'input': [], 'output': []}
-for i in range(len(nums)-6):
-  ds['input'].append(nums[i:i+5])
-  ds['output'].append(nums[i+5])
-
-print(f'len of input: {len(ds["input"])}, len of output: {len(ds["output"])}')
-print(f'first few: {ds["input"][:5], ds["output"][:5]}')
-
-#%% train using pytorch
 from torch import nn
 import torch.nn.functional as F
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import time
+import datetime
+import re
 
+#%% prepare some raw text with only lower case letters, digits and underscore
+def make_text(filename):
+  with open(filename, 'r') as file:
+    text = file.read()
+  return re.sub('[^a-zA-Z0-9 ]', '', text).lower()
+
+text = make_text('rod_steiger.txt')
+print(f'len of text: {len(text)}, text[:20]: {text[:20]}')
+
+vocab = sorted(list(set(text)))
+print(f'len of vocab: {len(vocab)}\n\nvocab: {vocab}\n\ncount: {Counter(text)}')
+
+#%% create the dataset with numericalisation - version 3
+def numericalise(vocab, text):
+  nums = []
+  for i in range(len(text)):
+    index = vocab.index(text[i])
+    nums.append(index)
+  return nums
+
+def make_ds(nums, seq_len):
+  ds = { 'input': [], 'output': []}
+  for i in range(len(nums) - seq_len - 1):
+    ds['input'].append(nums[i:i+seq_len])
+    ds['output'].append(nums[i+seq_len])
+  return ds
+
+sequence_length = 5
+nums = numericalise(vocab, text)
+ds = make_ds(nums, sequence_length)
+print(f'len of input: {len(ds["input"])}, len of output: {len(ds["output"])}')
+print(f'first few: {ds["input"][:7], ds["output"][:7]}')
+
+#%% train using pytorch
 class TrainEmbeddings(nn.Module):
-  def __init__(self, vocab_size, embed_size, input_size, hidden_nodes):
+  def __init__(self, vocab_size, embed_size, seq_len, hidden_nodes):
     super().__init__()
     self.embeddings = nn.Embedding(vocab_size, embed_size)
-    self.layer1 = nn.Linear(embed_size * input_size, hidden_nodes)
+    self.layer1 = nn.Linear(embed_size * seq_len, hidden_nodes)
     self.layer2 = nn.Linear(hidden_nodes, hidden_nodes)
     self.output = nn.Linear(hidden_nodes, vocab_size)
     self.debug = True
@@ -74,15 +60,14 @@ class TrainEmbeddings(nn.Module):
     # if self.debug: print(f'res 1: {res}')
     res = F.relu(self.layer1(res))
     res = F.relu(self.layer2(res))
-    # if self.debug: print(f'res 2: {res}')
     res = self.output(res)
     # if self.debug: print(f'res 3: {res}')
     self.debug = False
     return res.view(len(x), -1)
 
 # device = ("cuda" if torch.cuda.is_available() else "cpu")
-device = 'cpu'
-def train(dl, model, loss_fn, optimizer):
+device = 'cuda'
+def train(dl, model, loss_fn, optimizer, epoch):
   for batch, (X, y) in enumerate(dl):
     X, y = X.to(device), y.to(device)
 
@@ -94,29 +79,28 @@ def train(dl, model, loss_fn, optimizer):
     loss.backward()
     optimizer.step()
 
-    if batch % 100 == 0:
-      current = (batch + 1) * len(X)
-      print(f'loss: {loss.item()}, {current}/{len(dl.dataset)}')
+    if epoch % 10 == 0:
+      print(f'loss: {loss.item()}')
 
 #%%
-# 1. Create dataset in a shape that works with pytorch trainer
-import torch
-from torch.utils.data import TensorDataset, DataLoader
-# .unsqueeze(1)?
+# Create a dataloader in a shape that works with pytorch trainer
 tds = TensorDataset(torch.tensor(ds['input']), torch.tensor(ds['output']))
-# 2. Create a dataloader from the above dataset
-dl = DataLoader(tds, batch_size=256)
+dl = DataLoader(tds, batch_size=1)
 # 3. Create a learner/model object that can be trained on the above dataloader
-model = TrainEmbeddings(len(vocab), 2, 5, 30)
+model = TrainEmbeddings(len(vocab),
+                        embed_size=2,
+                        seq_len=sequence_length,
+                        hidden_nodes=30).to(device)
 # 4. Create a suitable optimizer & a loss function
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=5)
+optimizer = torch.optim.SGD(model.parameters(), lr=5e-4)
 
-epochs = 250
+epochs = 10
+start = time.time()
 for i in range(epochs):
   print(f'Epoch {i}=======================')
-  train(dl, model, loss_fn, optimizer)
-print('Training done.')
+  train(dl, model, loss_fn, optimizer, epoch=i)
+print(f'Training took {str(datetime.timedelta(seconds=time.time() - start))}.')
 
 #%%
 import pandas as pd
@@ -124,10 +108,10 @@ import altair as alt
 embeddings = list(model.parameters())[0]
 print(f'len of embeddings: {len(embeddings)}, embeddings: {embeddings[:5]}')
 
-df = pd.DataFrame(embeddings.detach().numpy(), columns=['x', 'y'])
+df = pd.DataFrame(embeddings.cpu().detach().numpy(), columns=['x', 'y'])
 df['label'] = vocab
 
-scatter = alt.Chart(df).mark_circle(size=60).encode(
+scatter = alt.Chart(df).mark_circle().encode(
   x='x',
   y='y'
 ).interactive()
@@ -152,7 +136,35 @@ print(f'len tds: {len(tds)}, tds[0:3]: {tds[0:3]}')
 print(f'{torch.tensor([1,2,3])}')
 
 #%% flatten a tensor list
-
 t = torch.rand([1, 5, 2])
 print(f't.shape: {t.shape}, t: {t}')
 print(f't.unstack(): {t.view(-1)}')
+
+#%% create the dataset - version 1
+def make_ds1(text):
+  ds1 = []
+  # First few elements are not present in the input and last element is not present in the output
+  # alternatively add dummy elements at the start and the end of the text
+  for i in range(len(text)-1):
+    pair = (text[i], text[i+1])
+    ds1.append(pair)
+
+ds1 = make_ds1(text)
+print(f'len of ds1: {len(ds1)}, firt few: {ds1[:5]}')
+
+#%% create the dataset - version 2
+def make_ds2(text):
+  ds2 = { 'input': [], 'output': []}
+  for i in range(len(text)-1):
+    ds2['input'].append(text[i])
+    ds2['output'].append(text[i+1])
+
+ds2 = make_ds2(text)
+print(f'len of input: {len(ds2["input"])}, len of output: {len(ds2["output"])}')
+
+#%% make_text()
+  # text = text.lower()
+  # # print(text[:40])
+  # table = str.maketrans('\n\t -ˈ—éəɪɡ–', '___________', string.punctuation)
+  # text = text.translate(table)
+  # return text
